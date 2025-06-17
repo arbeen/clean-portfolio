870a47028fa845b6d030aca9b0288c3243110d7a
Arbin Shrestha <arbinshrestha98@gmail.com>
2025-06-17 12:14:36 -0400

    reduced size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 import { Fade, Flex, Line, ToggleButton } from "@once-ui-system/core";
 
-import { routes, display, person, about, blog, work, gallery } from "@/resources";
+import { routes, display, person, about, blog, work } from "@/resources";
 import { ThemeToggle } from "./ThemeToggle";
 import styles from "./Header.module.scss";
 
@@ -129,23 +129,7 @@ export const Header = () => {
                   />
                 </>
               )}
-              {routes["/gallery"] && (
-                <>
-                  <ToggleButton
-                    className="s-flex-hide"
-                    prefixIcon="gallery"
-                    href="/gallery"
-                    label={gallery.label}
-                    selected={pathname.startsWith("/gallery")}
-                  />
-                  <ToggleButton
-                    className="s-flex-show"
-                    prefixIcon="gallery"
-                    href="/gallery"
-                    selected={pathname.startsWith("/gallery")}
-                  />
-                </>
-              )}
+             
               {display.themeSwitcher && (
                 <>
                   <Line background="neutral-alpha-medium" vert maxHeight="24" />
--- src/resources/once-ui.config.js
+++ src/resources/once-ui.config.js
@@ -8,7 +8,6 @@ const routes = {
   "/about": true,
   "/work": true,
   "/blog": true,
-  "/gallery": false,
 };
 
 const display = {
